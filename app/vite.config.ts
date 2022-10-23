b2995df237cfad9562162f50d928f94ecdb16e4c
Stephen Dause <stephendause@gmail.com>
2022-10-23 13:18:38 -0400

    working proxy
    I do not think pre-commit is working yet, since it is a Python tool.
    
    It may be that the best thing to do with that is make it run
    as a completely separate tool in a separate environment, if I can.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
+    // see https://vitejs.dev/config/server-options.html#server-proxy
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
   },
   build: {
     target: 'esnext',
