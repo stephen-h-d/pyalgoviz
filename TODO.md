af31bc6ce08f1ad5e16609e037d92a6210895875
Stephen Dause <stephendause@gmail.com>
2022-11-27 10:18:45 -0500

    Modify TODO.md, reorganize Visualizer

--- TODO.md
+++ TODO.md
@@ -1,13 +1,11 @@
 ### Ready-to-implement TODOs
 
-1. Highlight lines in algo and viz area
-    1. For both algo view and viz view:
-        1. Highlight with red when there's an error
-        2. Highlight with light blue-green (current color) when just going through normal animation
-            * Note: I will need to create and export different effects (e.g. `lineToHighlightChangedEffect`) for each one.
+0. Make HMR work with vanilla TS
+0. Make vite fail if TSC fails
+1. Finish up highlighting stuff
+    1. For viz view, highlight with red when there's an error
     2. When the doc changes, reset the line to highlight to -1.
 2. Support loading and saving all of the user's scripts
-3. Use basic EditorView themes
 4. Support running server-side in a sandboxed manner
 5. Support caching the results (once running server-side is implemented)
 6. Make a landing page with running algorithms.  (This will likely depend on the cache)
@@ -17,7 +15,6 @@
 10. Tell the user when Pyodide is loading, when it is done loading, when the code is running, and when it is done running.
 11. Separate the visualizer, animator, editorsMgr
     * Note: This will likely require one setup file (edit.ts) and separate files for each of those classes
-    * Note: It might also require a separate file for initializing editor views.  Either that or EditorsMgr initializes the editors.
 12. Disable all the buttons when:
     * pyodide is not loaded
     * a script is already running in pyodide
@@ -28,3 +25,5 @@
 ### Planning TODOs
 1. Make a plan for the layout.
 2. Decide which other login ID providers to support and make TODO items for those.
+3. Make unit tests?
+4. Make integration tests?
---
+++ src/visualizer.ts
@@ -0,0 +1,349 @@
+import { EditorView } from "codemirror";
+import { asyncRun } from "./py-worker";
+import { executorScript } from "./executor";
+import { Editor } from "./editor";
+import {select, arc} from "d3";
+
+function T(canvas: any,x,y,txt,size,font:string, color:string) {
+  canvas.append('text').attr('x', x)
+      .attr('y', y)
+      .text(txt)
+      .attr("font-size", ''+size+'px')
+      .attr("font-family", font)
+      .attr("fill", color);
+}
+
+function L(canvas: any,x1,y1,x2,y2,color,width) {
+  canvas.append('line')
+      .attr('x1', x1)
+      .attr('y1', y1)
+      .attr('x2', x2)
+      .attr('y2', y2)
+      .attr('stroke', color)
+      .attr('stroke-width', width);
+}
+
+function R(canvas: any,x,y,w,h,fill,border) {
+  canvas.append('rect')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('width', w)
+      .attr('height', h)
+      .attr('fill', fill)
+      .attr('stroke', border);
+}
+
+function C(canvas: any,x,y,r,fill,border) {
+  canvas.append('circle')
+      .attr('cx', x)
+      .attr('cy', y)
+      .attr('r', r)
+      .attr('fill', fill)
+      .attr('stroke', border);
+}
+
+function A(canvas: any, x,y,r1,r2,start,end,color) {
+  canvas.append('path')
+      .attr('d',
+              arc()
+              .innerRadius(r1)
+              .outerRadius(r2)
+              .startAngle(start)
+              .endAngle(end)
+      )
+      .attr('transform', "translate("+x+','+y+")")
+      .attr('fill', color);
+}
+
+// TODO put this somewhere
+// function doVizHelp() {
+//   if ($('#stopButton span').html() == 'Stop') {
+//       doStop()
+//   }
+//   function showHelp() {
+//     outputArea.setValue(
+//         "Python Algorithm Visualization Help\n" +
+//         "--------------------------------------------------------------\n\n" +
+//         "The visualization script in the bottom left visualizes the code in the top left while it runs. " +
+//         "You can refer to all local variables used in the algorithm above. " +
+//         "If an undefined local or other error is reached, the visualization script stops. " +
+//         "Enable 'Show Errors' to show the errors causing the visualization script to stop. " +
+//         "\n" +
+//         "\nThe visualization script is executed once for each executed line in the algorithm. " +
+//         "When the script runs, you can check the value of <b>__lineno__</b> to conditionally run " +
+//         "a subset of your script to make the visualization act more like a breakpoint. " +
+//         "\nYou can include arbitrary Python code, including defining helper functions." +
+//         "\n" +
+//         "\nAvailable values/primitives:" +
+//         "\n * __lineno__" +
+//         "\n * beep(frequency, milliseconds)" +
+//         "\n * text(x, y, txt, size=13, font='Arial', color='black')" +
+//         "\n * line(x1, y1, x2, y2, color='black', width=1)" +
+//         "\n * rect(x, y, w, h, fill='white', border='black')" +
+//         "\n * circle(x, y, radius, fill='white', border='black')" +
+//         "\n * arc(cx, cy, innerRadius, outerRadius, startAngle, endAngle, color='black')" +
+//         "\n * barchart(x, y, w, h, items, highlight=-1, scale=1, " +
+//         "\n                                   fill='black', border='black')" +
+//         ""
+//     )
+//   }
+//   setTimeout(showHelp, 1)
+// }
+
+const EDITOR_WIDTH = 600; // TODO make this more dynamic
+const EDITOR_HEIGHT = 450; // TODO make this more dynamic
+const RENDERING_SCALE = 1.0; // TODO make this more dynamic
+
+const DELAY = new Map([
+    ['Fast', 1],
+    ['Medium', 10],
+    ['MediumSlow', 25],
+    ['Slow', 50],
+    ['Snail', 200],
+    ['Molasses', 1000],
+]);
+
+const STEPS = new Map([
+    ['Fast', 20],
+    ['Medium', 100],
+    ['MediumSlow', 150],
+    ['Slow', 200],
+    ['Snail', 400],
+    ['Molasses', 800],
+]);
+
+class Animator {
+  private timerId: number;
+  private currentEvent: number = 0;
+  private paused: boolean = false;
+
+  public constructor(
+    private readonly outputArea: EditorView,
+    private readonly playPauseButton: HTMLButtonElement,
+    private readonly speedSelect: HTMLSelectElement,
+    private readonly progressDiv: HTMLDivElement,
+    private readonly renderAreaDiv: HTMLDivElement,
+    private readonly events: Array<[number,string,string]>,
+    private readonly lastError: string,
+    private readonly setCurrentLine: (currentLine: number)=> void,
+  ) {
+    this.timerId = this.play();
+  }
+
+  public pause() {
+    this.speedSelect.disabled = false;
+    this.paused = true;
+    // TODO this is not quite right since play/pause is a different concept than run; fix it
+    this.playPauseButton.innerText = "Play";
+    window.clearInterval(this.timerId);
+  }
+
+  public showRendering(script: string, w: number, h: number) {
+    if (script) {
+      this.renderAreaDiv.innerHTML = '';
+      const svg = select(this.renderAreaDiv)
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+      const canvas = svg
+        .append("g")
+        .attr("transform", "scale(" + RENDERING_SCALE + ")");
+
+      try {
+        eval(script);
+      } catch (e) {
+        T(canvas, 100, 100, "INTERNAL ERROR: ", 15, "Arial", "red");
+        T(canvas, 100, 120, "" + e, 15, "Arial", "red");
+        T(canvas, 100, 140, "" + script, 15, "Arial", "black");
+      }
+    }
+  }
+
+  public showEvent() {
+    const e = this.events[this.currentEvent];
+    try {
+
+    this.setCurrentLine(e[0]);
+
+    this.progressDiv.innerText = "Step " + (this.currentEvent + 1) + " of " + this.events.length;
+
+    //   $("#slider").slider("value", this.currentEvent);  TODO make slider
+
+      let output = "";
+      for (let n = 0; n <= this.currentEvent; n++) {
+        output += this.events[n][2];
+      }
+      if (output) {
+        this.setOutputAreaText(output + this.lastError);
+      }
+    } catch (exc) {}
+    this.showRendering(e[1], EDITOR_WIDTH, EDITOR_HEIGHT - 5);
+  }
+
+  public goToNextStep() {
+    this.pause();
+    if (this.currentEvent < this.events.length - 1) {
+      this.currentEvent += 1;
+      this.showEvent();
+    }
+  }
+
+  public goToPreviousStep() {
+    this.pause();
+    if (this.currentEvent > 0) {
+      this.currentEvent -= 1;
+      this.showEvent();
+    }
+  }
+
+  public togglePaused() {
+    if (this.paused) {
+      if (this.currentEvent >= this.events.length - 1) {
+        this.currentEvent = 0;
+      }
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+
+  public doAnimationStep() {
+    const speed = this.speedSelect.value || "MediumSlow";
+    const last = this.events.length - 1;
+    const step = Math.max(
+      1,
+      Math.round((Math.random() * this.events.length) / (STEPS.get(speed) || 25))
+    );
+    if (this.currentEvent < last) {
+      this.currentEvent = Math.min(this.currentEvent + step, last);
+      this.showEvent();
+    } else {
+      this.pause();
+    }
+  }
+
+  public play() {
+    this.paused = false;
+    this.speedSelect.disabled = true;
+    this.playPauseButton.innerText = "Pause";
+    const speed = this.speedSelect.value || "MediumSlow";
+    this.showEvent();
+    this.timerId = window.setInterval(() => this.doAnimationStep(), DELAY.get(speed) || 25);
+    return this.timerId;
+  }
+
+  private setOutputAreaText(text: string) {
+    // TODO figure out a way to reduce the duplication here... perhaps
+    // some functions that operate on `EditorView`s.
+    const transaction = this.outputArea.state.update({
+        changes: {
+          from: 0,
+          to: this.outputArea.state.doc.length,
+          insert: text,
+        },
+      });
+      const update = this.outputArea.state.update(transaction);
+      this.outputArea.update([update]);
+  }
+}
+
+export class Visualizer {
+  private animator: Animator | null = null;
+
+  public constructor(
+    private readonly algoEditor: Editor,
+    private readonly outputArea: EditorView,
+    private readonly vizEditor: Editor,
+    private readonly runButton: HTMLButtonElement,
+    private readonly playPauseButton: HTMLButtonElement,
+    private readonly speedSelect: HTMLSelectElement,
+    private readonly renderAreaDiv: HTMLDivElement,
+    private readonly progressDiv: HTMLDivElement
+  ) {
+  }
+
+  private setOutputAreaText(text: string) {
+    const transaction = this.outputArea.state.update({
+      changes: {
+        from: 0,
+        to: this.outputArea.state.doc.length,
+        insert: text,
+      },
+    });
+    const update = this.outputArea.state.update(transaction);
+    this.outputArea.update([update]);
+  }
+
+  public doPlayPause() {
+    if (this.animator !== null) {
+      this.animator.togglePaused();
+    } else {
+      console.error("Animator was unexpectedly null");
+    }
+  }
+
+  public doNextStep() {
+    if (this.animator !== null) {
+      this.animator.goToNextStep();
+    } else {
+      console.error("Animator was unexpectedly null");
+    }
+  }
+
+  public doPreviousStep() {
+    if (this.animator !== null) {
+      this.animator.goToPreviousStep();
+    } else {
+      console.error("Animator was unexpectedly null");
+    }
+  }
+
+  public async doRun() {
+    this.setOutputAreaText("Running...");
+
+    this.runButton.disabled = true;
+
+    const context = {
+      script: this.algoEditor.currentValue(),
+      viz: this.vizEditor.currentValue(),
+      showVizErrors: true,
+    };
+
+    try {
+      const { py_error, events } = await asyncRun(executorScript, context);
+
+      //   $("*").css("cursor", "auto");  // TODO change the cursors somehow?
+      this.runButton.disabled = false;
+
+      const py_error_msg = py_error.get("msg");
+      const py_error_lineno = py_error.get("lineno");
+      this.setOutputAreaText(py_error_msg);
+      // $("#slider").slider({ // TODO actually set up slider
+      //   value: 1,
+      //   step: 1,
+      //   min: 0,
+      //   max: this.events.length - 1,
+      //   slide: handleSlide,
+      // });
+      let lastError = "";
+      if (py_error_lineno > 0) {
+        lastError = py_error_msg;
+        this.algoEditor.setErrorLine(py_error_lineno);
+      }
+
+      if (events !== undefined) {
+        if (this.animator !== null) {
+          this.animator.pause();
+        }
+
+        this.animator = new Animator(this.outputArea, this.playPauseButton, this.speedSelect, this.progressDiv, this.renderAreaDiv, events, lastError, this.algoEditor.setHighlightLine.bind(this.algoEditor));
+      } else {
+        console.error("events undefined after calling asyncRun");
+      }
+    } catch (e) {
+      console.log(
+        `Error in pyodideWorker: ${e}`
+      );
+    }
+  }
+}
