4431b271295369774a62474cfdc947c4ad4df25b
Stephen Dause <stephendause@gmail.com>
2023-03-18 10:58:00 -0400

    rf resizing

--- src/edit3.tsx
+++ src/edit3.tsx
@@ -1,5 +1,5 @@
 /* @refresh reload */
-import { Accessor, createEffect, createSignal, Match, Switch} from 'solid-js';
+import { Accessor, createEffect, createSignal, Setter} from 'solid-js';
 import { render } from 'solid-js/web';
 import {LoadScriptDialog, SaveScriptDialog} from "./solid_load_dialog";
 import * as styles from "./edit3.css";
@@ -90,7 +90,7 @@ interface Tab {
     content: string;
   }
 
-function BottomRightContents() {
+function BottomRightContents(props: {vizLog: Accessor<string>, algoLog: Accessor<string>}) {
     const [hoveredTab, setHoveredTab] = createSignal<number | null>(null);
     const [selectedTab, setSelectedTab] = createSignal<number>(1);
 
@@ -158,25 +158,35 @@ function BottomRightContents() {
 
 export default BottomRightContents;
 
-function IDE(props: {ref: any,getSelf:() => HTMLDivElement}){
+class Resizer{
+    private cell_11_height: Accessor<number>;
+    private set_cell_11_height: Setter<number>;
+    private cell_12_height: Accessor<number>;
+    private cell_21_height: Accessor<number>;
+    private set_cell_21_height: Setter<number>;
+    private cell_22_height: Accessor<number>;
+    private col_1_width: Accessor<number>;
+    private set_col_1_width: Setter<number>;
+    private col_2_width: Accessor<number>;
 
-    const [cell_11_height, set_cell_11_height] = createSignal(40);
-    const cell_12_height = () => 100 - cell_11_height();
-    const [cell_21_height, set_cell_21_height] = createSignal(70);
-    const cell_22_height = () => 100 - cell_21_height();
-    const [col_1_width, set_col_1_width] = createSignal(50);
-    const col_2_width = () => 100 - col_1_width();
+    public constructor(private mainDivAcc: Accessor<HTMLDivElement>) {
+        [this.cell_11_height, this.set_cell_11_height] = createSignal(40);
+        this.cell_12_height = () => 100 - this.cell_11_height();
+        [this.cell_21_height, this.set_cell_21_height] = createSignal(70);
+        this.cell_22_height = () => 100 - this.cell_21_height();
+        [this.col_1_width, this.set_col_1_width] = createSignal(50);
+        this.col_2_width = () => 100 - this.col_1_width();
+    }
 
-    function resize_col_listener(ev: MouseEvent) {
+    public resize_col_listener(ev: MouseEvent) {
           ev.preventDefault();
-          const ideDiv = props.getSelf();
-          const original_ide_rect = ideDiv.getBoundingClientRect();
+          const original_ide_rect = this.mainDivAcc().getBoundingClientRect();
           const total_width = original_ide_rect.width;
-          const left_x = ideDiv.getBoundingClientRect().x;
+          const left_x = this.mainDivAcc().getBoundingClientRect().x;
 
-          function mouse_moved(ev: MouseEvent) {
+          const mouse_moved = (ev: MouseEvent) => {
             const newFirstColPct = Math.min(Math.max((ev.pageX - left_x) / total_width * 100, 5), 95);
-            set_col_1_width(newFirstColPct);
+            this.set_col_1_width(newFirstColPct);
           }
 
           document.addEventListener("mousemove", mouse_moved); // TODO debounce this
@@ -185,15 +195,14 @@ function IDE(props: {ref: any,getSelf:() => HTMLDivElement}){
           });
     };
 
-    function create_resize_cell_listener(set_top_cell_height: (val: number) => void){
-        function resize_cell_listener(ev: MouseEvent) {
+    private create_resize_cell_listener(set_top_cell_height: (val: number) => void){
+        const resize_cell_listener = (ev: MouseEvent) => {
             ev.preventDefault();
-            const ideDiv = props.getSelf();
-            const original_ide_rect = ideDiv.getBoundingClientRect();
+            const original_ide_rect = this.mainDivAcc().getBoundingClientRect();
             const total_height = original_ide_rect.height;
-            const top_y = ideDiv.getBoundingClientRect().y;
+            const top_y = this.mainDivAcc().getBoundingClientRect().y;
 
-            function mouse_moved(ev: MouseEvent) {
+            const mouse_moved = (ev: MouseEvent) => {
               const newHeightPct = Math.min(Math.max((ev.pageY - top_y) / total_height * 100, 5), 95);
               set_top_cell_height(newHeightPct);
             }
@@ -206,38 +215,60 @@ function IDE(props: {ref: any,getSelf:() => HTMLDivElement}){
       return resize_cell_listener;
     }
 
-    function getCellStyle(){
-        return {
-            "--cell-11-height": `${cell_11_height()}%`,
-            "--cell-12-height": `${cell_12_height()}%`,
-            "--cell-21-height": `${cell_21_height()}%`,
-            "--cell-22-height": `${cell_22_height()}%`,
-            "--col-1-width": `${col_1_width()}%`,
-            "--col-2-width": `${col_2_width()}%`,
-        }
+    public resize_cell_11_listener() {
+        return this.create_resize_cell_listener(this.set_cell_11_height.bind(this));
     }
 
-    return <div ref={props.ref} class={styles.ide} style={getCellStyle()}>
+    public resize_cell_21_listener() {
+        return this.create_resize_cell_listener(this.set_cell_21_height.bind(this));
+    }
+
+    public getCellStyle(){
+        return {
+            "--cell-11-height": `${this.cell_11_height()}%`,
+            "--cell-12-height": `${this.cell_12_height()}%`,
+            "--cell-21-height": `${this.cell_21_height()}%`,
+            "--cell-22-height": `${this.cell_22_height()}%`,
+            "--col-1-width": `${this.col_1_width()}%`,
+            "--col-2-width": `${this.col_2_width()}%`,
+        }
+    }
+}
+
+interface ResizerArgs {
+    initialContents: string,
+    extensions: Array<Extension>,
+    textReadOnly: boolean,
+}
+
+function IDE(props: {ref: any, getSelf:() => HTMLDivElement}){
+    const [vizLog, setVizLog] = createSignal("viz log contents...");
+    const [algoLog, setAlgoLog] = createSignal("algo log contents...");
+
+    const resizer = new Resizer(props.getSelf);
+    // const resize_col_listener = resizer.re
+
+    return <div ref={props.ref} class={styles.ide} style={resizer.getCellStyle()}>
         <div class={styles.left_col}>
-            <div class={styles.right_edge} onmousedown={resize_col_listener}></div>
+            <div class={styles.right_edge} onmousedown={resizer.resize_col_listener.bind(resizer)}></div>
             <div class={styles.top_left_cell} >
                 <TopLeftContents/>
-                <div class={styles.bottom_edge} onmousedown={create_resize_cell_listener(set_cell_11_height)}></div>
+                <div class={styles.bottom_edge} onmousedown={resizer.resize_cell_11_listener()}></div>
             </div>
             <div class={styles.bottom_left_cell}>
                 <BottomLeftContents/>
-                <div class={styles.top_edge} onmousedown={create_resize_cell_listener(set_cell_11_height)}></div>
+                <div class={styles.top_edge} onmousedown={resizer.resize_cell_11_listener()}></div>
             </div>
         </div>
         <div class={styles.right_col}>
-            <div class={styles.left_edge} onmousedown={resize_col_listener}></div>
+            <div class={styles.left_edge} onmousedown={resizer.resize_col_listener.bind(resizer)}></div>
             <div class={styles.top_right_cell}>
                 <TopRightContents/>
-                <div class={styles.bottom_edge} onmousedown={create_resize_cell_listener(set_cell_21_height)}></div>
+                <div class={styles.bottom_edge} onmousedown={resizer.resize_cell_21_listener()}></div>
             </div>
             <div class={styles.bottom_right_cell}>
-                <BottomRightContents/>
-                <div class={styles.top_edge} onmousedown={create_resize_cell_listener(set_cell_21_height)}></div>
+                <BottomRightContents algoLog={algoLog} vizLog={vizLog}/>
+                <div class={styles.top_edge} onmousedown={resizer.resize_cell_21_listener()}></div>
             </div>
         </div>
     </div>
