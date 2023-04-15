460b3cda7366a7cfb512a64bfbf0bb4e75d24c5c
Stephen Dause <stephendause@gmail.com>
2023-04-15 19:39:23 -0400

    rm old files

--- generate_code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-rm -rf generated/*
-pnpm run tsc --build tsconfig-generator.json
-node generator/ts-out/generator/run.js
--- generator/run.ts
+++
@@ -1,7 +0,0 @@
-
-import {page_object as edit_page_object} from "../src/containers/edit_page.js";
-import {page_object as load_dialog_object} from "../src/containers/load_dialog.js";
-import { genCode } from "./genCode.js";
-
-genCode(edit_page_object, "classes");
-genCode(load_dialog_object, "load_dialog_classes");
