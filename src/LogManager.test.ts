1109c3bc7cba9bfcf46b139a914c0df0eee7b0b7
Stephen Dause <stephendause@gmail.com>
2023-04-22 10:43:54 -0400

    set up testing

---
+++ src/LogManager.test.ts
@@ -0,0 +1,47 @@
+import * as fc from 'fast-check';
+import { VizEvent } from './exec_result';
+import { LogManager } from './LogManager';
+
+describe('LogManager', () => {
+  test('getAlgoLogUntilIndex should return concatenated algo logs up to the given index', () => {
+    fc.assert(
+      fc.property(fc.array(fc.record({
+        lineno: fc.nat(),
+        viz_output: fc.string(),
+        viz_log: fc.string(),
+        algo_log: fc.string()
+      })), fc.nat(), (events: VizEvent[], index: number) => {
+        if (events.length === 0) return true; // Skip empty events array
+
+        index = index % events.length; // Ensure index is within bounds
+        const logManager = new LogManager(events);
+
+        const expected = events.slice(0, index + 1).map(e => e.algo_log).join('\n');
+        const actual = logManager.getAlgoLogUntilIndex(index);
+
+        return expected === actual;
+      })
+    );
+  });
+
+  test('getVizLogUntilIndex should return concatenated viz logs up to the given index', () => {
+    fc.assert(
+      fc.property(fc.array(fc.record({
+        lineno: fc.nat(),
+        viz_output: fc.string(),
+        viz_log: fc.string(),
+        algo_log: fc.string()
+      })), fc.nat(), (events: VizEvent[], index: number) => {
+        if (events.length === 0) return true; // Skip empty events array
+
+        index = index % events.length; // Ensure index is within bounds
+        const logManager = new LogManager(events);
+
+        const expected = events.slice(0, index + 1).map(e => e.viz_log).join('\n');
+        const actual = logManager.getVizLogUntilIndex(index);
+
+        return expected === actual;
+      })
+    );
+  });
+});
