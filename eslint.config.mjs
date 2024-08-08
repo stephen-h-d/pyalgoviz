36896499d8285831d6f34746839661f7ee01504e
stephen-h-d <stephendause@gmail.com>
2024-08-08 18:39:45 -0400

    get eslint solid plugin working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,29 @@
-import js from "@eslint/js";
-import solid from "eslint-plugin-solid/configs/typescript.js";
-import * as tsParser from "@typescript-eslint/parser";
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import solid from 'eslint-plugin-solid';
 
-export default tseslint.config({
-  files: ['src/**/*.ts'],
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.strict,
-  ],
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-});
+export default [
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'solid': solid,
+      'prettier': prettier
+    },
+    rules: {
+      'solid/reactivity': 'error',
+      'solid/no-destructure': 'error',
+      'solid/jsx-no-undef': 'error'
+    }
+  }
+];
